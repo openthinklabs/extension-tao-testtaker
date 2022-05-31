9afa312499193ee5780c4bb315ab2aae3eb5162b
Wildan Maulana <wildan.m@gmail.com>
2022-05-31 16:21:38 +0700

    search test-taker by label

--- actions/ApiCustom.php
+++ actions/ApiCustom.php
@@ -55,4 +55,28 @@ class ApiCustom extends Api
           return json_encode(['success' => false, 'errorMsg' => $message]);
         }
     }
+
+    /**
+     * Search testtaker by label
+     */
+    public function search() {
+        //harusnya menggunakan methode ini, tapi sementara kita tembak langsung databasenya
+        //http://cbt.unand/taoTestTaker/TestTaker/getOntologyData?extension=taoTestTaker&perspective=TestTaker&section=manage_test_takers&classUri=http%3A%2F%2Fwww.tao.lu%2FOntologies%2FTAOSubject.rdf%23Subject&hideInstances=0&filter=*&offset=0&limit=30&selected=http_2_tao_0_local_1_mytao_0_rdf_3_i6295c0b3e603443900057a92d7e1875207
+        
+        try { 
+        $sql = "select subject as testtaker_uri from statements where object='{$this->getRequestParameter('label')}'";
+        $stmt = $this->getPersistence()->query($sql);
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if(count($data) > 0) { 
+            return $this->returnSuccess([
+                'success' => true,
+                'testtaker_uri' =>  $data['testtaker_uri']
+            ], false);             
+        }
+        } catch (Exception $e) { 
+            $message = $this->returnFailure($e);
+            return json_encode(['success' => false, 'errorMsg' => $message]);               
+        }
+    }    
 }
